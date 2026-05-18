be301f5b651d717ceebf990c2ad666b44e06500d
MD-Developer-yt <animeworldday2005@gmail.com>
2026-05-18 08:34:07 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
-
-# ------------------------- #
-# Don't Remove Credit 
-# Ask Doubt @AU_Bot_Discussion 
-# Owner @Mr_Mohammed_29 
-# ------------------------- #
